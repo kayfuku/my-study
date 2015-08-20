bcd90b15d1109b814fcdb556072de84ecb30f756
kayfuku <kayfuku@users.noreply.github.com>
2015-08-19 18:25:32 -0700

    format

--- Android/01_BubbleActivity/Circle.java
+++ Android/01_BubbleActivity/Circle.java
@@ -8,55 +8,55 @@
 
 package com.example.graphicsdemo;
 
-public class Circle 
+public class Circle
 {
-	private int x, y, r, col;
-	
-	// x accessor.
-	public int getX()
-	{
-		return x;
-	}
-    
-	// y accessor.
-	public int getY()
-	{
-		return y;
-	}
-	
-	// r accessor.
-	public int getR()
-	{
-		return r;
-	}
-	
-	// col accessor.
-	public int getColor()
-	{
-		return col;
-	}
-	
-	// x mutator.
-	public void setX(int x)
-	{
-		this.x = x;
-	}
-	
-	// y mutator.
-	public void setY(int y)
-	{
-		this.y = y;
-	}
-	
-	// r mutator.
-	public void setR(int r)
-	{
-		this.r = r;
-	}
-	
-	// col mutator.
-	public void setColor(int col)
-	{
-		this.col = col;
-	}
+    private int x, y, r, col;
+
+    // x accessor.
+    public int getX()
+    {
+        return x;
+    }
+
+    // y accessor.
+    public int getY()
+    {
+        return y;
+    }
+
+    // r accessor.
+    public int getR()
+    {
+        return r;
+    }
+
+    // col accessor.
+    public int getColor()
+    {
+        return col;
+    }
+
+    // x mutator.
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    // y mutator.
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    // r mutator.
+    public void setR(int r)
+    {
+        this.r = r;
+    }
+
+    // col mutator.
+    public void setColor(int col)
+    {
+        this.col = col;
+    }
 }
