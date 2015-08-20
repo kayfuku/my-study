2f523f1d4635453110d371759dfb775d9215c5a1
kayfuku <kayfuku@users.noreply.github.com>
2015-08-19 18:35:36 -0700

    format

--- Android/03_SendBirthdayWish/HappyGuy.java
+++ Android/03_SendBirthdayWish/HappyGuy.java
@@ -9,66 +9,66 @@
 
 package com.example.sendbirthdaywish;
 
-public class HappyGuy 
+public class HappyGuy
 {
-	private String name;
-	private String sms;
-	private String email;
-	private String birthDate;
-	
-	public HappyGuy()
-	{
-		name = new String("");
-		sms = new String("");
-		email = new String("");
-		birthDate = new String("");		
-	}
-	
-	public HappyGuy(String n, String s, String e, String b)
-	{
-		name = new String(n);
-		sms = new String(s);
-		email = new String(e);
-		birthDate = new String(b);		
-	}
-	
-	public String getName()
-	{
-		return name;
-	}
-	
-	public String getSMS()
-	{
-		return sms;
-	}
-	
-	public String getEmail()
-	{
-		return email;
-	}
-	
-	public String getBirthDate()
-	{
-		return birthDate;
-	}
-	
-	public void setName(String n)
-	{
-		name = n;
-	}
-	
-	public void setSMS(String s)
-	{
-		sms = s;
-	}
-	
-	public void setEmail(String e)
-	{
-		email = e;
-	}
-	
-	public void setBirthDate(String b)
-	{
-		birthDate = b;
-	}
+    private String name;
+    private String sms;
+    private String email;
+    private String birthDate;
+
+    public HappyGuy()
+    {
+        name = new String("");
+        sms = new String("");
+        email = new String("");
+        birthDate = new String("");
+    }
+
+    public HappyGuy(String n, String s, String e, String b)
+    {
+        name = new String(n);
+        sms = new String(s);
+        email = new String(e);
+        birthDate = new String(b);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSMS()
+    {
+        return sms;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getBirthDate()
+    {
+        return birthDate;
+    }
+
+    public void setName(String n)
+    {
+        name = n;
+    }
+
+    public void setSMS(String s)
+    {
+        sms = s;
+    }
+
+    public void setEmail(String e)
+    {
+        email = e;
+    }
+
+    public void setBirthDate(String b)
+    {
+        birthDate = b;
+    }
 }
