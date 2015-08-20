60c2d096fea0534b1f5f653fae7be8f018c7a9f8
kayfuku <kayfuku@users.noreply.github.com>
2015-08-19 18:47:15 -0700

    format

--- Java/Java_EE/02_DemoJDBC.java
+++ Java/Java_EE/02_DemoJDBC.java
@@ -26,30 +26,29 @@ public class DemoJDBC
     private static final String DB_NAME = "- - -";
     private static final String DB_USER_NAME = "- - -";
     private static final String DB_PASSWORD = "- - -";
-    
+
     private static final String FILE_NAME = "data/US_states";
-    private static HashMap<String, String> statesCapitalsMap = 
-                                                 new HashMap<>();
+    private static HashMap<String, String> statesCapitalsMap = new HashMap<>();
 
     private static final String DB_TABLE = "states";
     private static final String DB_COLUMN_ID = "id";
     private static final String DB_COLUMN_STATE = "state";
     private static final String DB_COLUMN_CAPITAL = "capital";
 
-    //-------------- readFile() --------------------------------
+    // -------------- readFile() --------------------------------
     // Read the file.
     private static File readFile()
     {
         // The file path is from the root directory of the project.
         File file = new File(FILE_NAME);
-        
+
         if (!file.exists())
             die(FILE_NAME + " does not exist.");
         if (file.isDirectory())
             die(FILE_NAME + " is a directory.");
         if (!file.canRead())
             die("You cannot read " + FILE_NAME + " .");
-        
+
         return file;
     } // end of readFile().
 
@@ -84,67 +83,65 @@ public class DemoJDBC
             e.printStackTrace();
         }
     } // end of parseUSState().
-    
-    //-------------createTable()--------------------------------
+
+    // -------------createTable()--------------------------------
     // Create a table.
-    private static void createTable(Statement statement) 
-	                                         throws SQLException
+    private static void createTable(Statement statement) throws SQLException
     {
         String sqlcmd = "";
-        
+
         sqlcmd += "CREATE TABLE IF NOT EXISTS " + DB_TABLE + " (";
         sqlcmd += DB_COLUMN_ID + " int  not null auto_increment, ";
         sqlcmd += DB_COLUMN_STATE + " varchar(32)  not null, ";
         sqlcmd += DB_COLUMN_CAPITAL + " varchar(32)  not null, ";
         sqlcmd += "primary key(" + DB_COLUMN_ID + "))";
-        
+
         statement.execute(sqlcmd);
     } // end of createTable().
 
-    //-------------insert()-------------------------------------
+    // -------------insert()-------------------------------------
     // Insert data.
-    private static void insert(Statement statement) 
-                                             throws SQLException
+    private static void insert(Statement statement) throws SQLException
     {
         String sqlcmd = "";
-        
+
         for (Map.Entry<String, String> entry : statesCapitalsMap.entrySet())
         {
             sqlcmd = "";
             sqlcmd += "INSERT INTO " + DB_TABLE + " (";
             sqlcmd += DB_COLUMN_STATE + ", " + DB_COLUMN_CAPITAL + ") ";
-            sqlcmd += "VALUES (\'" + entry.getKey() + "\', \'" + 
-			          entry.getValue() + "\')";
+            sqlcmd += "VALUES (\'" + entry.getKey() + "\', \'" + entry.getValue()
+                    + "\')";
             statement.execute(sqlcmd);
-        }         
+        }
     } // end of insert().
-    
-    //-------------- main() ------------------------------------
-    // Program execution starts with this method.    
+
+    // -------------- main() ------------------------------------
+    // Program execution starts with this method.
     public static void main(String[] args) throws SQLException
     {
         Connection connection = null;
         Statement statement = null;
-        
-        try 
+
+        try
         {
             // Always begin with connection when using database!
-            connection = DriverManager.getConnection(
-                    DB_URL + DB_NAME, DB_USER_NAME, DB_PASSWORD);
+            connection = DriverManager.getConnection(DB_URL + DB_NAME, DB_USER_NAME,
+                    DB_PASSWORD);
             System.out.println("Connected to the database.");
-            
-            // PreparedStatement might be better 
-			// for preventing SQL Injection. 
+
+            // PreparedStatement might be better
+            // for preventing SQL Injection.
             statement = connection.createStatement();
             // Create a table.
             createTable(statement);
             System.out.println("Table created.");
-            // Get data from the file and parse it. 
+            // Get data from the file and parse it.
             File file = readFile();
             parseUsStates(file);
-            // Insert the data to the table If not yet. 
-            ResultSet resultSet = statement.executeQuery(
-                            "SELECT count(*) FROM " + DB_TABLE);
+            // Insert the data to the table If not yet.
+            ResultSet resultSet = statement.executeQuery("SELECT count(*) FROM "
+                    + DB_TABLE);
             resultSet.next();
             if (resultSet.getInt(1) < 50)
             {
@@ -154,43 +151,41 @@ public class DemoJDBC
 
             // Queries.
             // 1. The number of states.
-            resultSet = statement.executeQuery(
-                            "SELECT count(*) FROM " + DB_TABLE);
+            resultSet = statement.executeQuery("SELECT count(*) FROM " + DB_TABLE);
             resultSet.next();
             System.out.println("1. Number of states: ");
             System.out.println(resultSet.getInt(1));
 
             // 2. Capital of Florida.
-            resultSet = statement.executeQuery(
-                    "SELECT " + DB_COLUMN_CAPITAL + " FROM " + DB_TABLE +
-                    " WHERE " + DB_COLUMN_STATE + " = \'Florida\'");
+            resultSet = statement.executeQuery("SELECT " + DB_COLUMN_CAPITAL
+                    + " FROM " + DB_TABLE + " WHERE " + DB_COLUMN_STATE
+                    + " = \'Florida\'");
             resultSet.next();
             System.out.println("2. Capital of Florida: ");
             System.out.println(resultSet.getString(1));
 
             // 3. The states that begin with letter 'M'.
-            resultSet = statement.executeQuery(
-                    "SELECT " + DB_COLUMN_STATE + " FROM " + DB_TABLE +
-                    " WHERE " + DB_COLUMN_STATE + " regexp \'^M\'");
-            System.out.println(
-			        "3. The states that begin with letter 'M': ");
+            resultSet = statement.executeQuery("SELECT " + DB_COLUMN_STATE
+                    + " FROM " + DB_TABLE + " WHERE " + DB_COLUMN_STATE
+                    + " regexp \'^M\'");
+            System.out.println("3. The states that begin with letter 'M': ");
             while (resultSet.next())
             {
                 System.out.println(resultSet.getString(1));
             }
-            
+
             // 4. The states that consists of 5 characters.
-            resultSet = statement.executeQuery(
-                    "SELECT " + DB_COLUMN_STATE + " FROM " + DB_TABLE +
-                    " WHERE " + DB_COLUMN_STATE + " regexp \'^.{5}$\'");
-            System.out.println(
-			        "4. The states that consist of 5 characters: ");
+            resultSet = statement.executeQuery("SELECT " + DB_COLUMN_STATE
+                    + " FROM " + DB_TABLE + " WHERE " + DB_COLUMN_STATE
+                    + " regexp \'^.{5}$\'");
+            System.out.println("4. The states that consist of 5 characters: ");
             while (resultSet.next())
             {
                 System.out.println(resultSet.getString(1));
             }
-            
-            if (!resultSet.isClosed()) resultSet.close();
+
+            if (!resultSet.isClosed())
+                resultSet.close();
         }
         catch (Exception e)
         {
@@ -198,8 +193,10 @@ public class DemoJDBC
         }
         finally
         {
-            if (!statement.isClosed()) statement.close();
-            if (!connection.isClosed()) connection.close();
+            if (!statement.isClosed())
+                statement.close();
+            if (!connection.isClosed())
+                connection.close();
         }
     } // end of main().
 }
