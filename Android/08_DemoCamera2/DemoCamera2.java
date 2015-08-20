df0f1222b978da0597516a0e450de7eb5ea62c4c
kayfuku <kayfuku@users.noreply.github.com>
2015-08-19 18:39:18 -0700

    format

--- Android/08_DemoCamera2/DemoCamera2.java
+++ Android/08_DemoCamera2/DemoCamera2.java
@@ -32,147 +32,132 @@ import android.widget.TextView;
 
 public class DemoCamera2 extends Activity
 {
-	private static final int PICTURE_ACTIVITY_CODE = 1;
+    private static final int PICTURE_ACTIVITY_CODE = 1;
     private EditText et;
     private ImageView iv;
     private TextView tv;
-	// Flag for avoiding like double-clicking snap button. 
-    private boolean mIsTaken = false;  
+    // Flag for avoiding like double-clicking snap button.
+    private boolean mIsTaken = false;
     private String strFileName;
-	private File picFile;
-	private Bitmap capturedImage;
+    private File picFile;
+    private Bitmap capturedImage;
 
-	@Override
-	//------------- onCreate() ---------------------------------
+    @Override
+    // ------------- onCreate() ---------------------------------
     // Called when the Activity is first created.
-	protected void onCreate(Bundle savedInstanceState) 
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_demo_camera2);
-		
-		et = (EditText) findViewById(R.id.et);
-		iv = (ImageView) findViewById(R.id.iv);
-		tv = (TextView) findViewById(R.id.tv_name);
-		
-		// Button Snap.
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_camera2);
+
+        et = (EditText) findViewById(R.id.et);
+        iv = (ImageView) findViewById(R.id.iv);
+        tv = (TextView) findViewById(R.id.tv_name);
+
+        // Button Snap.
         Button btnSnap = (Button) findViewById(R.id.btn_snap);
         btnSnap.setOnClickListener(new View.OnClickListener()
         {
             @Override
-			//----- onClick() ----------------------------------
-			// Called when Snap button is hit.
+            // ----- onClick() ----------------------------------
+            // Called when Snap button is hit.
             public void onClick(View v)
             {
-            	if (!mIsTaken)
-            	{
-            	    // Avoid something like double-clicking 
-					// Snap button. 
-            		mIsTaken = true;
-            		
-            		// Get the file name.
-            		strFileName = makeFileName();
-            		picFile = new File(
-            			Environment.getExternalStorageDirectory()
-						+ "/test", 
-            			strFileName);
-        			
-            		// Implicit intent. Invoke the app 
-					// that has a camera function.
-            		Intent intent = 
-					    new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            		Uri outputFileUri = Uri.fromFile(picFile);
-        			intent.putExtra(MediaStore.EXTRA_OUTPUT, 
-					                outputFileUri);
-        			startActivityForResult(intent, 
-					                       PICTURE_ACTIVITY_CODE);
-            	}
+                if (!mIsTaken)
+                {
+                    // Avoid something like double-clicking
+                    // Snap button.
+                    mIsTaken = true;
+
+                    // Get the file name.
+                    strFileName = makeFileName();
+                    picFile = new File(Environment.getExternalStorageDirectory()
+                            + "/test", strFileName);
+
+                    // Implicit intent. Invoke the app
+                    // that has a camera function.
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    Uri outputFileUri = Uri.fromFile(picFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+                    startActivityForResult(intent, PICTURE_ACTIVITY_CODE);
+                }
             }
         });
-        
+
         // Button Show.
         Button btnShow = (Button) findViewById(R.id.btn_show);
         btnShow.setOnClickListener(new View.OnClickListener()
         {
             @Override
-			//----- onClick() ----------------------------------
-			// Called when Show button is hit.
+            // ----- onClick() ----------------------------------
+            // Called when Show button is hit.
             public void onClick(View v)
             {
-            	iv.setImageBitmap(capturedImage);
-            	
-				// Resize and rotate the image.
-				Matrix mtrx = new Matrix();
-				iv.setScaleType(ScaleType.MATRIX);
-				mtrx.postRotate(90.0f, 
-				                iv.getWidth()/2, 
-				                iv.getHeight()/2);
-				iv.setImageMatrix(mtrx);
-				
-				tv.setText(strFileName);
+                iv.setImageBitmap(capturedImage);
+
+                // Resize and rotate the image.
+                Matrix mtrx = new Matrix();
+                iv.setScaleType(ScaleType.MATRIX);
+                mtrx.postRotate(90.0f, iv.getWidth() / 2, iv.getHeight() / 2);
+                iv.setImageMatrix(mtrx);
+
+                tv.setText(strFileName);
             }
         });
-	} // end of onCreate().
+    } // end of onCreate().
 
-	@Override
-	//------------- onResume() ---------------------------------
+    @Override
+    // ------------- onResume() ---------------------------------
     // Called when the Activity comes forward again.
-	protected void onResume()
-	{
-		super.onResume();
-		mIsTaken = false;
-	}
-	
-	@Override
-	//------------- onActivityResult() -------------------------
-	protected void onActivityResult(int requestCode, 
-		                            int resultCode, 
-		                            Intent data) 
-	{	
-		if(requestCode == PICTURE_ACTIVITY_CODE)
-		{
-			if (resultCode == RESULT_OK)
-			{	
-				try 
-				{
-					FileInputStream in = 
-					    new FileInputStream(picFile);
-					BitmapFactory.Options options = 
-					    new BitmapFactory.Options();
-					options.inSampleSize = 7;
-					capturedImage = 
-					    BitmapFactory.decodeStream(in,
-							                       null,
-							                       options);
-				} 
-				catch (FileNotFoundException e)
-				{
-					e.printStackTrace();
-				}
-				
-				mIsTaken = false;
-			}
-		}
-	} // end of onActivityResult().
-	
-	//------------- makeFileName() -----------------------------
-	// Make a file name.
-	private String makeFileName()
-	{
-		String strFileName;
-		
-		if (et.getText().toString().equals(""))
-		{
-			// Get the current time for the file name.
-			Calendar cal = Calendar.getInstance();
-			SimpleDateFormat formatter = 
-			    new SimpleDateFormat("yyyyMMdd_HHmmss");
-		    strFileName = formatter.format(cal.getTime()) + ".jpg";
-		}
-		else
-		{
-			strFileName = et.getText().toString() + ".jpg";
-		}
-		
-		return strFileName;
-	}
+    protected void onResume()
+    {
+        super.onResume();
+        mIsTaken = false;
+    }
+
+    @Override
+    // ------------- onActivityResult() -------------------------
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICTURE_ACTIVITY_CODE)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                try
+                {
+                    FileInputStream in = new FileInputStream(picFile);
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 7;
+                    capturedImage = BitmapFactory.decodeStream(in, null, options);
+                }
+                catch (FileNotFoundException e)
+                {
+                    e.printStackTrace();
+                }
+
+                mIsTaken = false;
+            }
+        }
+    } // end of onActivityResult().
+
+    // ------------- makeFileName() -----------------------------
+    // Make a file name.
+    private String makeFileName()
+    {
+        String strFileName;
+
+        if (et.getText().toString().equals(""))
+        {
+            // Get the current time for the file name.
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd_HHmmss");
+            strFileName = formatter.format(cal.getTime()) + ".jpg";
+        }
+        else
+        {
+            strFileName = et.getText().toString() + ".jpg";
+        }
+
+        return strFileName;
+    }
 }
